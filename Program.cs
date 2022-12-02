fc52617f63b92a92f7e94e7a47874aa51cb0ba28
unknown <bhotnikhil25@gmail.com>
2022-12-02 20:36:44 +0530

    [Nikhil]-Factor Program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Factors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, x;
+            Console.WriteLine("Enter the Number ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            for (x = 1; x <= num; x++)
+            {
+                if (num % x == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+    }
